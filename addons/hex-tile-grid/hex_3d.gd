a8b6c7e9a1f11802f4642c986e0375a2d0972819
thelvm <luckiler3@gmail.com>
2024-07-19 14:02:47 +0200

    Improved documentation

--- addons/hex-tile-grid/hex_3d.gd
+++ addons/hex-tile-grid/hex_3d.gd
@@ -1,16 +1,22 @@
+## A Hex that positions itself in 3D space.
+## The local Y axis is independent of the position on the hex grid and can be set manually.
 @tool
 @icon("res://addons/hex-tile-grid/hex_3d.svg")
 class_name Hex3D
 extends Node3D
 
-@export var axial_coord: Vector2:
+## The local hex grid axial coordinates.
+@export var axial_coord := Vector2(0, 0):
 	set(new_coords):
 		hex.axial_coordinates = new_coords
 		var cart_2D_coords := hex.cartesian_coordinates
 		position = Vector3(cart_2D_coords.x, position.y, cart_2D_coords.y)
 	get:
+		if Vector2(position.x, position.z) != hex.cartesian_coordinates:
+			hex.cartesian_coordinates = Vector2(position.x, position.z)
 		return hex.axial_coordinates
 
+## The size of the Hex. Directly affects position as the cartesian coordinates are influenced by both axial coordinates and size.
 @export var size: float = 1.0:
 	set(new_size):
 		hex.size = new_size
@@ -19,6 +25,7 @@ extends Node3D
 	get:
 		return hex.size
 
+## The underlying hex. Can be used to get other formats of the coordinaes. Editing positions directly to it is not recommended.
 var hex: Hex = Hex.new()
 
 
--- addons/hex-tile-grid/hex_tile_grid.gd
+++ addons/hex-tile-grid/hex_tile_grid.gd
@@ -5,7 +5,6 @@ extends RefCounted
 @export_storage var _tiles: Dictionary
 
 ## Adds a hex tile at the specified axial coordinates and returns a reference to the just added tile.
-## If a tile already exists at these coordinates, returns it instead
 func set_tile_at(coords: Vector2, new_tile: Hex) -> Hex:
 	_tiles[coords] = new_tile
 	return _tiles[coords]
