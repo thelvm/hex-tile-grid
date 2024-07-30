f60736d912ea42686f1278eb238eae696b8db476
thelvm <luckiler3@gmail.com>
2024-07-30 07:05:09 +0200

    Update hex_tile_map_3D.gd

--- addons/hex-tile-map/hex_tile_map_3D.gd
+++ addons/hex-tile-map/hex_tile_map_3D.gd
@@ -4,3 +4,9 @@ class_name HexTileMap3D
 extends Node3D
 
 var hex_grid: HexTileMap
+
+var _visual_hexs: Array[Hex3D]
+
+
+func set_tile(axial_position, ) -> void:
+	hex_grid.set_tile_at(axial_position)
